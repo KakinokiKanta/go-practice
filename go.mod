20e9c0d48a7006d201be93c04649c4b55b011185
KakinokiKanta <kanta12621@icloud.com>
2024-03-08 06:42:35 +0900

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KakinokiKanta/go-intermediate
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main () {
+	// helloHandlerという名前で、ハンドラを定義
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		// ハンドラの処理内容:
+		// 何がきても、"Hello, world!"の文字列を返す
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	// 定義したhelloHandlerを使うように登録
+	http.HandleFunc("/", helloHandler)
+
+	// サーバ起動時のログ出力
+	log.Println("server start at port 8080")
+
+	// ListenAndServe関数にて、サーバを起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
