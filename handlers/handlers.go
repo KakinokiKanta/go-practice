c6f60aa68bab90a09770821955354965c60715a8
KakinokiKanta <kanta12621@icloud.com>
2024-03-08 19:27:44 +0900

    /hello以外のエンドポイントに対しても、指定したHTTPメソッド以外は受け付けないように変更

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -17,27 +17,47 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 // TODO: ブログ記事の投稿をするためのハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // TODO: ブログ記事の一覧を取得するためのハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // TODO: 指定した記事ナンバーの投稿データを取得するためのハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID := 1
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
-	io.WriteString(w, resString)
+	if req.Method == http.MethodGet {
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // TODO: 記事にいいねをつけるためのハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // TODO: 記事にコメントを投稿するためのハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment...")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment...")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
