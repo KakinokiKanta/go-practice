86ac3f95f71702c2c1572b8b7e31958d928b3dc4
KakinokiKanta <kanta12621@icloud.com>
2024-03-08 17:16:53 +0900

    fix: 各種ハンドラをhandlersパッケージに移行

---
+++ handlers/handlers.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// /helloのハンドラ
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	// ハンドラの処理内容:
+	// 何がきても、"Hello, world!"の文字列を返す
+	io.WriteString(w, "Hello, world!\n")
+}
+
+// TODO: ブログ記事の投稿をするためのハンドラ
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article...\n")
+}
+
+// TODO: ブログ記事の一覧を取得するためのハンドラ
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article List\n")
+}
+
+// TODO: 指定した記事ナンバーの投稿データを取得するためのハンドラ
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID := 1
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+}
+
+// TODO: 記事にいいねをつけるためのハンドラ
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+// TODO: 記事にコメントを投稿するためのハンドラ
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...")
+}
--- main.go
+++ main.go
@@ -1,59 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/KakinokiKanta/go-intermediate/handlers"
 )
 
 func main () {
-	// helloHandlerという名前で、ハンドラを定義
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		// ハンドラの処理内容:
-		// 何がきても、"Hello, world!"の文字列を返す
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	// TODO: ブログ記事の投稿をするためのハンドラ
-	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Article...\n")
-	}
-
-	// TODO: ブログ記事の一覧を取得するためのハンドラ
-	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article List\n")
-	}
-
-	// TODO: 指定した記事ナンバーの投稿データを取得するためのハンドラ
-	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%d\n", articleID)
-		io.WriteString(w, resString)
-	}
-
-	// TODO: 記事にいいねをつけるためのハンドラ
-	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
-	}
-
-	// TODO: 記事にコメントを投稿するためのハンドラ
-	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Comment...")
-	}
-
 	// 定義したhelloHandlerを使うように登録
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
 	// ブログ記事の投稿をするためのエンドポイント
-	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
 	// ブログ記事の一覧を取得するためのエンドポイント
-	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
 	// 指定した記事ナンバーの投稿データを取得するためのエンドポイント
-	http.HandleFunc("/article/1", articleDetailHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
 	// 記事にいいねをつけるためのエンドポイント
-	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
 	// 記事にコメントを投稿するためのエンドポイント
-	http.HandleFunc("/comment", postCommentHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	// サーバ起動時のログ出力
 	log.Println("server start at port 8080")
