692962457d33a76202d85e99b958067cf2f83e33
KakinokiKanta <kanta12621@icloud.com>
2024-03-08 20:04:11 +0900

    update: gorilaパッケージを導入してmuxでルーティングするように変更

--- main.go
+++ main.go
@@ -5,25 +5,28 @@ import (
 	"net/http"
 
 	"github.com/KakinokiKanta/go-intermediate/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main () {
+	r := mux.NewRouter()
+
 	// 定義したhelloHandlerを使うように登録
-	http.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/hello", handlers.HelloHandler)
 	// ブログ記事の投稿をするためのエンドポイント
-	http.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
 	// ブログ記事の一覧を取得するためのエンドポイント
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
 	// 指定した記事ナンバーの投稿データを取得するためのエンドポイント
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
 	// 記事にいいねをつけるためのエンドポイント
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
 	// 記事にコメントを投稿するためのエンドポイント
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	// サーバ起動時のログ出力
 	log.Println("server start at port 8080")
 
 	// ListenAndServe関数にて、サーバを起動
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
