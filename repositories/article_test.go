311e645480d35d5b75831f044cb8af23357aa55c
KakinokiKanta <kanta12621@icloud.com>
2024-03-19 01:08:01 +0900

    add: testing.TのCleanupメソッドを用いて、InsertArticle関数のテストを追加

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -8,6 +8,31 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// InsertArticle関数のテスト
+func TestInsertArticle(t *testing.T) {
+	article := models.Article{
+		Title: "insertTest",
+		Contents: "testest",
+		UserName: "saki",
+	}
+
+	expectedArticleNum := 3
+	newArticle, err := repositories.InsertArticle(testDB, article)
+	if err != nil {
+		t.Error(err)
+	}
+	if newArticle.ID != expectedArticleNum {
+		t.Errorf("new article id is expected %d but got %d\n", expectedArticleNum, newArticle.ID)
+	}
+
+	t.Cleanup(func() {
+		const sqlStr = `
+			delete from articles where title = ? and contents = ? and username = ?;
+		`
+		testDB.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	})
+}
+
 // SelectArticleList関数のテスト
 func TestSelectArticleList(t *testing.T) {
 	// テスト対象の関数を実行
