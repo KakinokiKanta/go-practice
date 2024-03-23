d223c5639d6c615063a1aa69665cfc7918ab7f4f
KakinokiKanta <kanta12621@icloud.com>
2024-03-23 23:17:14 +0900

    update: main内でデータベース接続を行うように変更

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/KakinokiKanta/go-intermediate/controllers"
 	"github.com/KakinokiKanta/go-intermediate/services"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+// データベース接続のための情報を定義
+var (
+	dbUser     string
+	dbPassword string
+	dbDatabase string
+	dbConn     string
 )
 
 func main() {
-	ser := services.NewMyAppService([])
+	db, err := connectDB()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
 	r := mux.NewRouter()
 
@@ -30,4 +48,26 @@ func main() {
 
 	// ListenAndServe関数にて、サーバを起動
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// DB接続
+func connectDB() (*sql.DB, error) {
+	loadEnv()
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+	dbUser = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbDatabase = os.Getenv("DB_NAME")
+	dbConn = fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	if err != nil {
+		fmt.Println("fail to load .env file")
+	}
 }
