912ab844b4ae407107f2d424989d603db72f3b0d
KakinokiKanta <kanta12621@icloud.com>
2024-03-27 11:38:26 +0900

    add: 独自エラーの導入

---
+++ apperrors/error.go
@@ -0,0 +1,17 @@
+package apperrors
+
+// 独自エラーに含めるフィールドの定義
+type MyAppError struct {
+	// (フィールド名を省略した場合、型名がそのままフィールド名になる)
+	ErrCode        // レスポンスとログに表示するエラーコード
+	Message string // レスポンスに表示するエラーメッセージ
+	Err     error  // エラーチェーンのための内部エラー
+}
+
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
+}
