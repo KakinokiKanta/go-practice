64d37eee83e0358381bf88548c39e878a3557fa3
KakinokiKanta <kanta12621@icloud.com>
2024-03-28 21:01:22 +0900

    add: エラーハンドラ用の関数の枠を追加

---
+++ apperrors/errorHandler.go
@@ -0,0 +1,8 @@
+package apperrors
+
+import "net/http"
+
+// エラーが発生したときのレスポンス処理をここで一括で行う
+func ErrorHandler(w http.ResponseWriter, req *http.Request, err error) {
+	// エラーの種類を判別して、適切なhttpレスポンスを返す
+}
