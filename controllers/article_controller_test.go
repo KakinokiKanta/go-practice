7f793c91db64f273e6e9fa733c3e08970cbdce96
KakinokiKanta <kanta12621@icloud.com>
2024-03-29 16:46:56 +0900

    update: ArticleListHandlerメソッドのユニットテストを追加

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -1,1 +1,46 @@
 package controllers_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestArticleListHandler(t *testing.T) {
+	// テスト対象のハンドラメソッドに入れるinputを定義
+	var tests = []struct {
+		name       string
+		query      string
+		resultCode int
+	}{
+		{
+			name:       "number query",
+			query:      "1",
+			resultCode: http.StatusOK,
+		},
+		{
+			name:       "alphabet query",
+			query:      "aaa",
+			resultCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// ハンドラに渡す2つの引数 w http.ResponseWriter, req *http.Request を用意する
+			url := fmt.Sprintf("http://localhost:8080/article/list?page=%s", tt.query)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+
+			res := httptest.NewRecorder()
+
+			// テスト対象のハンドラメソッドからoutputを得る
+			aCon.ArticleListHandler(res, req)
+
+			// outputが期待通りがチェック
+			if res.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
+			}
+		})
+	}
+}
