fc5cc16d712bb032dbb886af88fcae878f8791d1
KakinokiKanta <kanta12621@icloud.com>
2024-04-02 19:30:18 +0900

    add: 自作ResponseWriterの追加

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,11 +5,36 @@ import (
 	"net/http"
 )
 
+// 自作ResponseWriterを作成
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+// コンストラクタ
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+// WriteHeaderメソッド
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
+// ロギングミドルウェア
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		// 自作ResponseWriterを作成
+		rlw := NewResLoggingWriter(w)
+
+		// ハンドラには自作ResponseWriterを渡す
+		next.ServeHTTP(rlw, req)
+
+		// 自作ResponseWriterからロギングしたいデータを出す
+		log.Println("res: ", rlw.code)
 	})
 }
