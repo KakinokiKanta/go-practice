8f07eeaa1f92ea45cca4f2935158af3c4f97b93e
KakinokiKanta <kanta12621@icloud.com>
2024-04-03 13:41:01 +0900

    update: go文wo

--- services/article_service.go
+++ services/article_service.go
@@ -37,21 +37,31 @@ func (s *MyAppService) GetArticleListService(page int) ([]models.Article, error)
 
 // 指定IDの記事情報を返却
 func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) {
+	var article models.Article
+	var commentList []models.Comment
+	var articleGetErr, commentGetErr error
+
 	// repositories層の関数SelectArticleDetailで記事の詳細を取得
-	article, err := repositories.SelectArticleDetail(s.db, articleID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			err = apperrors.NAData.Wrap(err, "no data")
+	go func() {
+		article, articleGetErr = repositories.SelectArticleDetail(s.db, articleID)
+	}()
+
+	// repositories層の関数SelectCommentListでコメント一覧を取得
+	go func() {
+		commentList, commentGetErr = repositories.SelectedCommentList(s.db, articleID)
+	}()
+
+	if articleGetErr != nil {
+		if errors.Is(articleGetErr, sql.ErrNoRows) {
+			err := apperrors.NAData.Wrap(articleGetErr, "no data")
 			return models.Article{}, err
 		}
-		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
+		err := apperrors.GetDataFailed.Wrap(articleGetErr, "fail to get data")
 		return models.Article{}, err
 	}
 
-	// repositories層の関数SelectCommentListでコメント一覧を取得
-	commentList, err := repositories.SelectedCommentList(s.db, articleID)
-	if err != nil {
-		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
+	if commentGetErr != nil {
+		err := apperrors.GetDataFailed.Wrap(commentGetErr, "fail to get data")
 		return models.Article{}, err
 	}
 
